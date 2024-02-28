548477b907608927c6f226c03c12fc9c0fcc1c2c
leloufadel <leloufadel@gmail.com>
2024-02-28 16:13:38 +0000

    Create device

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_28_144456) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_28_150912) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.decimal "total"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -34,5 +42,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_28_144456) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "carts", "users"
   add_foreign_key "products", "users"
 end
