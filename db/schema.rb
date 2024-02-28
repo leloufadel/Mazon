63ffdc318151f70e11d8e481896faccee38451c5
leloufadel <leloufadel@gmail.com>
2024-02-28 14:46:53 +0000

    Create Product table

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_28_144456) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.decimal "current_price"
+    t.decimal "previous_price"
+    t.string "image"
+    t.text "pro_description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "products", "users"
 end
