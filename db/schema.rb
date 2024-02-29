1e98bbf3db7065281caba2e5c9d44806603fd76b
Arch-Noize <santiagonoize@gmail.com>
2024-02-29 09:04:21 -0400

    Update User's table with Devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_28_150912) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_29_130218) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,9 +22,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_28_150912) do
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "cart_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_orders_on_cart_id"
+    t.index ["product_id"], name: "index_orders_on_product_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
-    t.string "subtitle"
     t.decimal "current_price"
     t.decimal "previous_price"
     t.string "image"
@@ -40,8 +48,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_28_150912) do
     t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "carts", "users"
+  add_foreign_key "orders", "carts"
+  add_foreign_key "orders", "products"
   add_foreign_key "products", "users"
 end
